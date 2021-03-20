4719b6100734429630474518fe964021db6c6b3d
insertcatchyphrases <81000326+insertcatchyphrases@users.noreply.github.com>
2021-03-20 00:06:02 -0400

    Add files via upload

---
+++ 6.5.py
@@ -0,0 +1,11 @@
+ext = "X-DSPAM-Confidence:    0.8475"
+pos = ext.find('0')
+print(float(ext[pos:pos+6]))
+
+#sppos = data.find (' ',atpos)
+#print(sppos)
+
+#host = data[atpos+1 : sppos]
+#print(hos
+ 
+
